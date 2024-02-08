595b25f6e7e3f007a3052b9ff32e95988b28baef
Joshua Matthews <jmatth93@gmail.com>
2024-02-08 01:20:56 -0600

    fix tongue animation executing only once:

--- scripts/frog.gd
+++ scripts/frog.gd
@@ -21,12 +21,14 @@ func _input(event):
 		if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
 			if tongue == null:
 				load_tongue()
-			add_child(tongue)
-			move_child(tongue, 0)
-			tongue.target_position = event.position
-			# get_angle_to get's the angle relative to the beginning position.
-			# so since I want to start with a -90 degree offset I need to account for that
-			angle_to_target = get_angle_to(event.position) - deg_to_rad(90)
+			if !tongue_animation_in_progress:
+				tongue_animation_in_progress = true
+				add_child(tongue)
+				move_child(tongue, 0)
+				tongue.target_position = event.position
+				# get_angle_to get's the angle relative to the beginning position.
+				# so since I want to start with a -90 degree offset I need to account for that
+				angle_to_target = get_angle_to(event.position) - deg_to_rad(90)
 
 func reset_rotation():
 	angle_to_target = deg_to_rad(-90)
@@ -36,6 +38,7 @@ func _on_body_entered(_body):
 	$CollisionShape2D.set_deferred("disabled", true)
 
 func _on_tongue_animation_done():
+	tongue_animation_in_progress = false
 	remove_child(tongue)
 	reset_rotation()
 
