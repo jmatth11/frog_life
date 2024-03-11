f4169822fcabfcfae32f452875950dfb9c1cba29
Joshua Matthews <jmatth93@gmail.com>
2024-03-11 18:00:49 -0500

    updated hud slightly, still need hud logic

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,9 +7,11 @@ var current_points = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GlobalState.update_score.connect(Callable(_on_player_fly_points))
-	$Score.text = "0"
+	var score_node = get_node("info_panel/score")
+	score_node.text = "0"
 	current_points = 0
 
 func _on_player_fly_points(num):
 	current_points += fly_points * num
-	$Score.text = str(current_points)
+	var score_node = get_node("info_panel/score")
+	score_node.text = str(current_points)
